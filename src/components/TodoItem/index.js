8428b019abc10ec9a8bb1f722cf15371c7e3a7ca
raveenkarri <131150835+raveenkarri@users.noreply.github.com>
2025-09-08 22:25:34 +0530

    Update index.js

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,43 +1,30 @@
-<<<<<<< HEAD
+import {useState} from 'react'
 import './index.css'
 
 const TodoItem = props => {
-  const {todoItem, onTodoListChange} = props
-  const {title, id} = todoItem
-
-  const onTodoListDelete = () => onTodoListChange(id)
-
-  return (
-    <li>
-      <p>{title}</p>
-      <button onClick={onTodoListDelete}>Delete</button>
-    </li>
-  )
-}
-=======
-import { useState } from 'react'
-import './index.css'
-
-const TodoItem = props => {
-  const { todoItem, onTodoListChange, onToggleComplete, onUpdateTitle } = props
-  const { title, id, completed } = todoItem
+  const {todoItem, onTodoListChange, onToggleComplete, onUpdateTitle} = props
+  const {title, id, completed} = todoItem
 
   const [isEditing, setIsEditing] = useState(false)
   const [editValue, setEditValue] = useState(title)
 
   const onSave = () => {
-    onUpdateTitle(id, editValue)
-    setIsEditing(false)
+    if (editValue.trim() !== '') {
+      onUpdateTitle(id, editValue)
+      setIsEditing(false)
+    }
   }
 
   return (
     <li className="todo-item">
+      {/* Checkbox to toggle complete */}
       <input
         type="checkbox"
         checked={completed}
         onChange={() => onToggleComplete(id)}
       />
 
+      {/* If editing show input, else show text */}
       {isEditing ? (
         <input
           type="text"
@@ -49,12 +36,18 @@ const TodoItem = props => {
         <p className={completed ? 'completed' : ''}>{title}</p>
       )}
 
+      {/* Edit / Save Button */}
       {isEditing ? (
-        <button onClick={onSave} className="save-btn">Save</button>
+        <button onClick={onSave} className="save-btn">
+          Save
+        </button>
       ) : (
-        <button onClick={() => setIsEditing(true)} className="edit-btn">Edit</button>
+        <button onClick={() => setIsEditing(true)} className="edit-btn">
+          Edit
+        </button>
       )}
 
+      {/* Delete Button */}
       <button onClick={() => onTodoListChange(id)} className="delete-btn">
         Delete
       </button>
@@ -62,5 +55,4 @@ const TodoItem = props => {
   )
 }
 
->>>>>>> solution
 export default TodoItem
